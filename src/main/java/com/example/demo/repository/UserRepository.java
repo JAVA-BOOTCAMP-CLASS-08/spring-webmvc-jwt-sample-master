ff48fc92a1f76508947d4068005ba1c1210157bc
andresaramos <andres.alberto.ramos@gmail.com>
2023-11-22 07:39:26 -0300

    First commmit

---
+++ src/main/java/com/example/demo/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package com.example.demo.repository;
+
+import com.example.demo.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import java.util.Optional;
+
+@RepositoryRestResource(path = "users", collectionResourceRel = "users", itemResourceRel = "user")
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByUsername(String username);
+
+}
